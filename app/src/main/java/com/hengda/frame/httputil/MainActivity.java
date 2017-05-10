585e296c6385744d9b687e986f931ea44daa17be
tailyou <tailyou@163.com>
2017-05-10 12:26:03 +0800

    20170510

--- app/src/main/java/com/hengda/frame/httputil/MainActivity.java
+++ app/src/main/java/com/hengda/frame/httputil/MainActivity.java
@@ -46,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 RetrofitHelper.getInstance()
                         .loadDatas()
+                        .doOnSubscribe(new Consumer<Disposable>() {
+                            @Override
+                            public void accept(Disposable disposable) throws Exception {
+                                compositeDisposable.add(disposable);
+                            }
+                        })
                         .subscribe(new Consumer<DataBean>() {
                             @Override
                             public void accept(DataBean dataBean) throws Exception {
@@ -64,7 +70,42 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnDownload).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                download();
+                RxDownload.getInstance().context(MainActivity.this)
+                        .maxThread(16).maxRetryCount(3)
+                        .download(url, saveName, savePath)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnSubscribe(new Consumer<Disposable>() {
+                            @Override
+                            public void accept(Disposable disposable) throws Exception {
+                                compositeDisposable.add(disposable);
+                                tvDownloadStatus.setText("下载地址：" + url + "\n");
+                                SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
+                                tvDownloadStatus.setText(tvDownloadStatus.getText() + "\n开始下载：" + sdf.format(new Date()));
+                            }
+                        })
+                        .doOnNext(new Consumer<DownloadStatus>() {
+                            @Override
+                            public void accept(DownloadStatus downloadStatus) throws Exception {
+                                tvDownloadPrg.setText("下载进度：" + downloadStatus.getFormatStatusString());
+                            }
+                        })
+                        .doOnError(new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                tvDownloadStatus.setText("下载失败:" + throwable.getMessage());
+                            }
+                        })
+                        .doOnComplete(new Action() {
+                            @Override
+                            public void run() throws Exception {
+                                SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
+                                tvDownloadPrg.setText(tvDownloadPrg.getText() + "\n下载完成：" + sdf.format(new Date()));
+                                File file = new File(savePath, saveName);
+                                file.delete();
+                            }
+                        })
+                        .subscribe();
             }
         });
 
@@ -73,46 +114,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //取消下载
         compositeDisposable.dispose();
-    }
-
-    private void download() {
-        RxDownload.getInstance().context(this).maxThread(16).maxRetryCount(3)
-                .download(url, saveName, savePath)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(new Consumer<Disposable>() {
-                    @Override
-                    public void accept(Disposable disposable) throws Exception {
-                        compositeDisposable.add(disposable);
-                        tvDownloadStatus.setText("下载地址：" + url + "\n");
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
-                        tvDownloadStatus.setText(tvDownloadStatus.getText() + "\n开始下载：" + sdf.format(new Date()));
-                    }
-                })
-                .doOnNext(new Consumer<DownloadStatus>() {
-                    @Override
-                    public void accept(DownloadStatus downloadStatus) throws Exception {
-                        tvDownloadPrg.setText("下载进度：" + downloadStatus.getFormatStatusString());
-                    }
-                })
-                .doOnError(new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        tvDownloadStatus.setText("下载失败:" + throwable.getMessage());
-                    }
-                })
-                .doOnComplete(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
-                        tvDownloadPrg.setText(tvDownloadPrg.getText() + "\n下载完成：" + sdf.format(new Date()));
-                        File file = new File(savePath, saveName);
-                        file.delete();
-                    }
-                })
-                .subscribe();
     }
 
 }
